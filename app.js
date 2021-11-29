216aa1152745f0bc499dfb5f60bcad7363bfc88e
robiul26 <robiulrobi26@gmail.com>
2021-11-29 22:41:24 +0600

    deploy

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
-let greenIcon = document.getElementById('green');
-let blackIcon = document.getElementById('black');
-let chocolateIcon = document.getElementById('chocolate');
+// For Dark and Light Mode
 let body = document.querySelector("body");
 let input = document.querySelectorAll("input");
 let buttons = document.querySelectorAll("button");
+let modeIcons = document.querySelector(".colors");
+let lightModeIcon = document.querySelector(".colors #white");
+let darkModeIcon = document.querySelector(".colors #black");
 
 
-greenIcon.addEventListener("click", greenColor);
-blackIcon.addEventListener("click", blackColor);
-chocolateIcon.addEventListener("click", chocolateColor);
+lightModeIcon.addEventListener("click", whiteColor);
+darkModeIcon.addEventListener("click", blackColor);
 
 
-function greenColor() {
-    body.style.background = "forestgreen";
-    // remove chocolate
-    input.forEach(x => {
-        x.classList.remove("chocolateInput");
-    });
-    buttons.forEach(x => {
-        x.classList.remove("chocolateBtn");
-    });
+function whiteColor() {
+    body.style.background = "lightgray";
+    lightModeIcon.style.display = "none";
+    darkModeIcon.style.display = "block";
     // remove black
     input.forEach(x => {
         x.classList.remove("blackInput");
@@ -32,164 +27,145 @@ function greenColor() {
 
 function blackColor() {
     body.style.background = "black";
+    lightModeIcon.style.display = "block";
+    darkModeIcon.style.display = "none";
     input.forEach(x => {
         x.classList.add("blackInput");
     });
     buttons.forEach(x => {
         x.classList.add("blackBtn");
     });
-
-    // remove chocolate
-    input.forEach(x => {
-        x.classList.remove("chocolateInput");
-    });
-    buttons.forEach(x => {
-        x.classList.remove("chocolateBtn");
-    });
-
 }
 
 
-function chocolateColor() {
-    body.style.background = "chocolate";
-    buttons.forEach(x => {
-        x.classList.add("chocolateBtn");
-    });
-    input.forEach(x => {
-        x.classList.add("chocolateInput");
-    });
+// For Submit Form
+// Get the Ui Element
+let btnSubmit = document.getElementById('bookForm');
+let bookList = document.getElementById('bookList');
 
-    // remove classes
-    input.forEach(x => {
-        x.classList.remove("blackInput");
-    });
-    buttons.forEach(x => {
-        x.classList.remove("blackBtn");
-    });
-}
-
-
-// Define UI Element
-let taskSubmitBtn = document.getElementById("taskSubmitBtn");
-
-let taskInput = document.getElementById("taskInput");
-let tasklist = document.getElementById("tasklist");
-let taskClearBtn = document.getElementById("taskClearBtn");
-let taskFilter = document.getElementById("taskFilter");
-
-
-// Define EvenListener
-taskSubmitBtn.addEventListener("click", addTask);
-tasklist.addEventListener("click", removeTask);
-taskClearBtn.addEventListener("click", clearTasks);
-taskFilter.addEventListener("keyup", filterTask);
-document.addEventListener('DOMContentLoaded', getTasks);
-
-
-// Define function
-// Add Task
-function addTask(e) {
-    e.preventDefault();
-
-    if (taskInput.value == "") {
-        alert("Please add task")
-    } else {
-        let li = document.createElement("li");
-        let output = taskInput.value;
-
-        tasklist.appendChild(li);
-        li.innerText = `${output} `;
-        let link = document.createElement("a");
-        link.setAttribute("href", "#");
-
-        link.innerText = "Delete";
-        li.appendChild(link);
-
-        storeTaskInLocalStorage(taskInput.value);
-        taskInput.value = '';
+// Book Class
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
     }
 }
 
-// Remove Task
-function removeTask(e) {
-    if (e.target.hasAttribute("href")) {
-        if (confirm("Are you sure!")) {
-            let ele = e.target.parentElement;
-            ele.remove();
-            removeFromLS(ele);
+
+// UI Class
+class UI {
+    static showAlert(message, className) {
+        let p = document.createElement('p');
+        p.className = `alert ${className}`;
+        p.appendChild(document.createTextNode(message));
+        let alertDiv = document.getElementById('alert');
+        alertDiv.appendChild(p);
+
+        setTimeout(() => {
+            document.querySelector('.alert').remove();
+        }, 3000);
+    }
+
+    static addToBookList(book) {
+        let list = document.querySelector('#bookList');
+        bookList.innerHTML += `
+        <tr>
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td>
+            <a href="#" class="deleteBtn">X</a>
+            </td>
+        </tr>
+        `;
+    }
+    static clearFields() {
+        document.getElementById('title').value = "";
+        document.getElementById('author').value = "";
+        document.getElementById('isbn').value = "";
+    }
+
+    static deleteBook(target) {
+        if (target.hasAttribute('href')) {
+            if (confirm('Are you sure!')) {
+                target.parentElement.parentElement.remove();
+                Store.removeBook(target.parentElement.previousElementSibling.textContent.trim());
+                UI.showAlert('Book removed!', 'success');
+            };
         }
     }
 }
 
-// Clear Tasks
-function clearTasks(e) {
-    tasklist.innerText = "";
-    localStorage.clear();
-}
-
-// Filter Task
-function filterTask(e) {
-    let text = e.target.value.toLowerCase();
-
-    var allLi = document.querySelectorAll("li");
-    allLi.forEach(task => {
-        let item = task.firstChild.textContent;
-        if (item.toLocaleLowerCase().indexOf(text) != -1) {
-            task.style.display = "block";
+// Local Storage Class
+class Store {
+    static getBooks() {
+        let books;
+        if (localStorage.getItem('books') === null) {
+            books = [];
         } else {
-            task.style.display = "none";
+            books = JSON.parse(localStorage.getItem('books'));
         }
-    });
-
-}
-
-// Store in Local Storage
-function storeTaskInLocalStorage(task) {
-    let tasks;
-    if (localStorage.getItem('tasks') === null) {
-        tasks = [];
-    } else {
-        tasks = JSON.parse(localStorage.getItem('tasks'));
+        return books;
     }
 
-
-    tasks.push(task);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-
-}
-
-function getTasks() {
-
-    let tasks;
-    if (localStorage.getItem('tasks') === null) {
-        tasks = [];
-    } else {
-        tasks = JSON.parse(localStorage.getItem('tasks'));
-    }
-    console.log(tasks);
-    tasks.forEach(item => {
-        tasklist.innerHTML += `<li>${item} <a href="#">Delete</a></li>`;
-    });
-}
-
-function removeFromLS(taskItem) {
-    let tasks;
-    if (localStorage.getItem('tasks') === null) {
-        tasks = [];
-    } else {
-        tasks = JSON.parse(localStorage.getItem('tasks'));
+    static addBook(book) {
+        let books = Store.getBooks();
+        books.push(book);
+        localStorage.setItem('books', JSON.stringify(books));
     }
 
-    let li = taskItem;
+    static displayBooks() {
+        let books = Store.getBooks();
+        books.forEach(book => {
+            UI.addToBookList(book);
+        });
+    }
 
-    li.removeChild(li.lastChild);   //<a>x</a>
+    static removeBook(isbn) {
+        let books = Store.getBooks();
+        books.forEach((book, index) => {
+            if (book.isbn === isbn) {
+                books.splice(index, 1);
+            }
+        });
 
-    tasks.forEach((task, index) => {
-        if (li.textContent.trim() === task) {
-            tasks.splice(index, 1);
-        }
-    });
-
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-
+        localStorage.setItem('books', JSON.stringify(books));
+    }
 }
+
+
+
+// Define AddEvenListener
+btnSubmit.addEventListener('submit', newBook);
+bookList.addEventListener('click', removeBook);
+document.addEventListener('DOMContentLoaded', Store.displayBooks());
+// Define Functions
+function newBook(e) {
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let isbn = document.getElementById('isbn').value;
+
+
+    if (title == "" || author == "" || isbn == "") {
+        UI.showAlert('Please fill all the fields!', 'error');
+    } else {
+        let book = new Book(title, author, isbn);
+        // console.log(book);
+        UI.showAlert('Book added!', 'success');
+        UI.addToBookList(book);
+        UI.clearFields();
+        Store.addBook(book);
+    }
+    e.preventDefault();
+}
+
+
+function removeBook(e) {
+    // console.log(e.target);
+    UI.deleteBook(e.target);
+    e.preventDefault();
+}
+
+
+
